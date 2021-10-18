33c65ca74389bcc74ac9805bab2430d645bdf325
Thomas Girault <thomas@girault.fr>
2021-10-18 02:10:45 +0200

    concatenates all the labels accross datasets for multitask learning + refactoring

--- train.py
+++ train.py
@@ -30,28 +30,83 @@ topics = [
     "New World Order",
     "Satanism",
 ]
+outpath = Path("/tmp/")
+
 #%%
 
-def task1(model_name, data_path):
+
+def task1_dataset(data_path, shuffle=False):
     cols = "id class text".split()
     df = pd.read_csv(data_path, sep=",", names=cols)
     df["class"] = df["class"] - 1
-    df = df.sample(frac=1)
+    if shuffle:
+        df = df.sample(frac=1)
+    df.to_csv(outpath / "task1-train.csv", index=False)
 
-    # l = len(df)
-    # train_split, eval_split = int(l * 0.8), int(l * 0.9)
 
-    outpath = Path("/tmp/corpus/")
-    df.to_csv(outpath / "train.csv", index=False)
+def task1_multilabels_dataset(data_path, shuffle=False):
+    cols = "id class text".split()
+    df = pd.read_csv(data_path, sep=",", names=cols)
+    bin_labels = pd.get_dummies(df["class"])
+    bin_labels.columns = classes
+    df = pd.concat([df, bin_labels], axis=1)
 
-    # df.iloc[:train_split].to_csv(outpath / "train.csv", index=False)
-    # df.iloc[train_split:eval_split].to_csv(outpath / "valid.csv", index=False)
-    # df.iloc[eval_split:].to_csv(outpath / "test.csv", index=False)
+    if shuffle:
+        df = df.sample(frac=1)
+    df.to_csv(outpath / "task1-train.csv", index=False)
+    return classes
+
+
+def task2_dataset(data_path, shuffle=False):
+    cols = ["id"] + topics + ["text"]
+    df = pd.read_csv(data_path, sep=",", names=cols)
+    if shuffle:
+        df = df.sample(frac=1)
+    df.to_csv(outpath / "task2-train.csv", index=False)
+
+
+def task3_dataset(data_path, shuffle=False):
+    cols = ["id"] + topics + ["text"]
+    df = pd.read_csv(data_path, sep=",", names=cols)
+
+    enc = OneHotEncoder()
+    X = enc.fit_transform(df[topics]).toarray().astype(int)
+    bin_cols = [f"{t} + {c}" for c in classes for t in topics]
+    bin_labels = pd.DataFrame(X, columns=bin_cols)
+    df = pd.concat([df, bin_labels], axis=1)
+    if shuffle:
+        df = df.sample(frac=1)
+
+    df.to_csv(outpath / "task3-train.csv", index=False)
+    return bin_cols
+
+
+def multitask_dataset(data_path: Path):
+    cols1 = task1_multilabels_dataset(data_path / "dev-1-task-1.csv")
+    task2_dataset(data_path / "dev-1-task-2.csv")
+    bin_cols = task3_dataset(data_path / "dev-1-task-3.csv")
+
+    df1 = pd.read_csv(outpath / "task1-train.csv")
+    df2 = pd.read_csv(outpath / "task2-train.csv")
+    df3 = pd.read_csv(outpath / "task3-train.csv")
+
+    df = pd.concat(
+        [df1[cols1], df2[topics], df3[bin_cols + ["text"]]], axis=1
+    )
+    df.to_csv(outpath / "multitask-train.csv", index=False)
+    return cols1 + topics + bin_cols
+
+
+def build_train(data_path, labels, model_name):
+    multilabels = True
+    if len(labels) == 1:
+        labels = labels[0]
+        multilabels = False
 
     datamodule = TextClassificationData.from_csv(
         "text",
-        "class",
-        train_file=str(outpath / "train.csv"),
+        labels,
+        train_file=data_path,
         # val_file=str(outpath / "valid.csv"),
         val_split=0.2,
         backbone=model_name,
@@ -64,81 +119,33 @@ def task1(model_name, data_path):
         metrics=[F1(datamodule.num_classes), MatthewsCorrcoef(datamodule.num_classes)],
         optimizer=torch.optim.AdamW,
         serializer=Labels(multi_label=True),
+        multi_label=multilabels,
     )
 
     trainer = flash.Trainer(max_epochs=25)
     trainer.finetune(model, datamodule=datamodule, strategy="freeze_unfreeze")
     trainer.test()
-    # trainer.save_checkpoint(f"{model_name}-{dataset_name}.pt")
-    # return model
+
+
+def task1(model_name, data_path):
+    # task1_dataset(data_path)
+    cols = task1_multilabels_dataset(data_path)
+    build_train(str(outpath / "task1-train.csv"), cols, model_name)
 
 
 def task2(model_name, data_path):
-    outpath = Path("/tmp/corpus/")
-    cols = ["id"] + topics + ["text"]
-    df = pd.read_csv(data_path, sep=",", names=cols)
-    df = df.sample(frac=1)
-    df.to_csv(outpath / "task2-train.csv", index=False)
-
-    datamodule = TextClassificationData.from_csv(
-        "text",
-        topics,
-        train_file=str(outpath / "task2-train.csv"),
-        val_split=0.2,
-        backbone=model_name,
-        batch_size=64,
-    )
-
-    # 2. Build the task
-    model = TextClassifier(
-        backbone=model_name,
-        num_classes=datamodule.num_classes,
-        metrics=[F1(datamodule.num_classes), MatthewsCorrcoef(datamodule.num_classes)],
-        optimizer=torch.optim.AdamW,
-        multi_label=True,
-    )
-
-    # 3. Create the trainer and finetune the model
-    trainer = flash.Trainer(max_epochs=25, gpus=torch.cuda.device_count())
-    trainer.finetune(model, datamodule=datamodule, strategy="freeze_unfreeze")
-    trainer.test()
+    task2_dataset(data_path)
+    build_train(str(outpath / "task2-train.csv"), topics, model_name)
 
 
 def task3(model_name, data_path):
-    outpath = Path("/tmp/corpus/")
-    cols = ["id"] + topics + ["text"]
-    df = pd.read_csv(data_path, sep=",", names=cols)
+    bin_cols = task3_dataset(data_path)
+    build_train(str(outpath / "task3-train.csv"), bin_cols, model_name)
 
-    enc = OneHotEncoder()
-    X = enc.fit_transform(df[topics]).toarray().astype(int)
-    bin_cols = [f"{t} + {c}" for c in classes for t in topics]
-    bin_labels = pd.DataFrame(X, columns=bin_cols)
-    df = pd.concat([df, bin_labels], axis=1)
-    df = df.sample(frac=1)
 
-    df.to_csv(outpath / "task3-train.csv", index=False)
-
-    datamodule = TextClassificationData.from_csv(
-        "text",
-        bin_cols,
-        train_file=str(outpath / "task3-train.csv"),
-        val_split=0.2,
-        backbone=model_name,
-    )
-
-    # 2. Build the task
-    model = TextClassifier(
-        backbone=model_name,
-        num_classes=datamodule.num_classes,
-        metrics=[F1(datamodule.num_classes), MatthewsCorrcoef(datamodule.num_classes)],
-        optimizer=torch.optim.AdamW,
-        multi_label=True,
-    )
-
-    # 3. Create the trainer and finetune the model
-    trainer = flash.Trainer(max_epochs=25, gpus=torch.cuda.device_count())
-    trainer.finetune(model, datamodule=datamodule, strategy="freeze_unfreeze")
-    trainer.test()
+def multitask(model_name, data_path):
+    cols = multitask_dataset(Path(data_path))
+    build_train(str(outpath / "multitask-train.csv"), cols, model_name)
 
 
 #%%
@@ -151,3 +158,4 @@ if __name__ == "__main__":
     import fire
 
     fire.Fire()
+# %%
