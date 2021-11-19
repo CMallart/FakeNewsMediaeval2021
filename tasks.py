8d37032a7c96de05a576bfc75537aa33b193d2bd
Thomas Girault <thomas@girault.fr>
2021-11-19 07:09:10 +0100

    fix and simplify output_predictions

--- tasks.py
+++ tasks.py
@@ -1,8 +1,10 @@
+#%%
 from pathlib import Path
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 
+#%%
 class Task:
     task_name = "task"
     multilabels = True
@@ -13,7 +15,7 @@ class Task:
         pass
 
     @classmethod
-    def output_prediction(probas, run_id):
+    def output_prediction(cls, probas, run_id):
         pass
 
     @classmethod
@@ -43,12 +45,17 @@ class Task1(Task):
 
         df["class"] = df["class"] - 1
         return df
-    def output_prediction(test_id, test_proba, run_id=1):
+
+    @classmethod
+    def output_prediction(cls, test_id, test_proba, run_id=1):
         # save the output in the task for
-        # probas = np.array(self.model.predict(df_test.text))
-        y_pred = np.array([np.argmax(probi)+1 if max(probi) > 0.2 else 0 for probi in test_proba]) #np.argmax(probas, axis=1)
-        df = pd.DataFrame(list(zip(test_id , y_pred)))
-        df.to_csv("ME21FND_IRISA_00{}.csv".format(run_id), index=False, header=False)
+        # probas = np.array(cls.model.predict(df_test.text))
+        y_min = np.full((test_proba.shape[0], 1), 0.2)
+        y_pred = np.hstack([y_min, test_proba]).argmax(axis=1)
+        df = pd.DataFrame(list(zip(test_id, y_pred)))
+        outpath = f"./experiments/ME21FND_IRISA_{cls.task_name}_{run_id}.csv"
+        df.to_csv(outpath, index=False, header=False)
+
 
 class Task2(Task):
     task_name = "task-2"
@@ -71,23 +78,33 @@ class Task2(Task):
         for l in Task2.labels:
             df[l] = pd.to_numeric(df[l])
         return df
-    
-    def output_prediction(test_id, test_proba, run_id=1):
-        # save the output in the task for
-        def proba_to_class(prob):
-            if prob<0.20:return 0
-            elif prob == 0.20: return -1
-            else: return 1
+
+    @classmethod
+    def output_prediction(cls, test_id, test_proba, run_id=1):
         test_id = list(test_id)
-        y_pred = np.array([[test_id[i]]+[proba_to_class(pi) for pi in test_proba[i]]for i in range(len(test_proba))]) #np.argmax(probas, axis=1)
-        df = pd.DataFrame(y_pred)#list(zip(test_id , y_pred)))
-        # df = pd.to_numeric(df)
-        df.to_csv("ME21FND_IRISA_10{}.csv".format(run_id), index=False, header=False)
+
+        def proba_to_class(p):
+            if p < 0.20:
+                return 0
+            elif p == 0.20:
+                return -1
+            else:
+                return 1
+
+        y_pred = np.vectorize(proba_to_class)(test_proba)
+        # test_proba = np.where(test_proba < 0.2, 0, test_proba)
+        # test_proba = np.where(test_proba == 0.2, -1, test_proba)
+        # test_proba = np.where(test_proba > 0.2, 1, test_proba)
+
+        df = pd.DataFrame(y_pred, columns=cls.labels)
+        df["id"] = test_id
+        outpath = f"./experiments/ME21FND_IRISA_{cls.task_name}_{run_id}.csv"
+        df[["id"] + cls.labels].to_csv(outpath, index=False, header=False)
 
 
 class Task3(Task):
     task_name = "task-3"
-    labels = [f"{t} + {c}" for t in Task2.labels for c in Task1.labels ]
+    labels = [f"{t} + {c}" for t in Task2.labels for c in Task1.labels]
 
     @classmethod
     def get_dataset(cls, data_path: Path):
@@ -98,21 +115,20 @@ class Task3(Task):
         X = enc.fit_transform(df[Task2.labels]).toarray().astype(int)
         bin_labels = pd.DataFrame(X, columns=Task3.labels)
         return pd.concat([df, bin_labels], axis=1)
-    
-    def output_prediction(test_id, test_proba, run_id=1):
-        # save the output in the task for
-        def split(lst, n=3):
-            for i in range(0, len(lst), n):
-                yield lst[i:i + n]
-        def proba_to_class(prob):
-            final_pred = []
-            return [np.argmax(splitted_proba) + 1 if max(splitted_proba) >= 0.20 else 0 for splitted_proba in split(prob)]
-    
+
+    @classmethod
+    def output_prediction(cls, test_id, test_proba, run_id=1):
         test_id = list(test_id)
-        y_pred = np.array([ [test_id[i]]+proba_to_class(test_proba[i])  for i in range(len(test_proba))]) #np.argmax(probas, axis=1)
-        df = pd.DataFrame(y_pred)#list(zip(test_id , y_pred)))
-        # df = pd.to_numeric(df)
-        df.to_csv("ME21FND_IRISA_20{}.csv".format(run_id), index=False, header=False)
+
+        y_min = np.full((test_proba.shape[0], 1), 0.2)
+        splits = np.split(test_proba, len(Task2.labels), axis=1)
+        y_pred = np.vstack(
+            [np.hstack([y_min, probas]).argmax(axis=1) for probas in splits]
+        ).T
+        df = pd.DataFrame(y_pred, columns=Task2.labels)
+        df["id"] = test_id
+        outpath = f"./experiments/ME21FND_IRISA_{cls.task_name}_{run_id}.csv"
+        df[["id"] + Task2.labels].to_csv(outpath, index=False, header=False)
 
 
 class MultiTasks(Task):
@@ -123,5 +139,15 @@ class MultiTasks(Task):
     def get_dataset(cls, data_path: Path):
         df1 = Task1.get_dataset(data_path)[Task1.labels]
         df2 = Task2.get_dataset(data_path)[Task2.labels]
-        df3 = Task3.get_dataset(data_path)[Task3.labels + ["text"]]
+        df3 = Task3.get_dataset(data_path)[Task3.labels + ["text", "id"]]
         return pd.concat([df1, df2, df3], axis=1)
+
+    @classmethod
+    def output_prediction(cls, test_id, test_proba, run_id=1):
+        l1, l2, l3 = len(Task1.labels), len(Task2.labels), len(Task3.labels)
+        splits = np.split(test_proba, [l1, l1 + l2, l1 + l2 + l3], axis=1)
+        Task1.output_prediction(test_id, splits[0], run_id)
+        Task2.output_prediction(test_id, splits[1], run_id)
+        Task3.output_prediction(test_id, splits[2], run_id)
+
+# %%
